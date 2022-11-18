74b09a224adea4fe9a5380b5656b731d58d26970
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-11-18 18:37:57 -0300

    adicionando o entitie account

---
+++ src/entities/Account.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Accounts")
+export class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "text" })
+  balance: string
+}
